dceac62e0b110b3a17c2af81f12f284a52ea59f3
swordsounds <126785544+swordsounds@users.noreply.github.com>
2023-10-17 13:26:49 -0230

    yuh

--- test.py
+++ test.py
@@ -1,38 +1,21 @@
-from guizero import App, Text, Slider
-
-# Action you would like to perform
-def counter():
-    text.value = int(slider.value)
-
-app = App("Hello world")
-text = Text(app, text="1")
-slider = Slider(app, start=1, end=5)
-text.repeat(10, counter)  # Schedule call to counter() every 1000ms
-app.display()
-
-
-# from guizero import *
-# import random
-
-
-# def read_sensor():
-#     return random.randrange(3200, 5310, 10) / 100
-
-
-# def update_label():
-#     text.set(read_sensor())
-#     # recursive call
-#     text.after(1000, update_label)
-
-
-# if __name__ == '__main__':
-#     app = App(title='Sensor Display!',
-#               height=100,
-#               width=200,
-#               layout='grid')
-
-#     title = Text(app, 'Sensor value:', grid=[0, 0])
-#     text = Text(app, "xx", grid=[1, 0])
-
-#     text.after(1000, update_label)
-#     app.display()
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        maxLength = 0
+        charMap = {}
+        left = 0
+        
+        for right in range(n):
+            if s[right] not in charMap or charMap[s[right]] < left:
+                charMap[s[right]] = right
+                maxLength = max(maxLength, right - left + 1)
+            else:
+                left = charMap[s[right]] + 1
+                charMap[s[right]] = right
+            print(f"ML:{maxLength} ", f"L:{left} ", f"R:{right}")
+        
+        return maxLength
+    
+solve = Solution()
+solve.lengthOfLongestSubstring("pwwkew")
+# print(solve.lengthOfLongestSubstring("pwwkew"))
