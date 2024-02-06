5a274d780c7c47f6b19ede8af7cbafd5d26d9f96
swordsounds <126785544+swordsounds@users.noreply.github.com>
2024-02-06 11:42:30 -0330

    Update Activity3.py

--- Stuff-from-APP(24)/U1Activties.py/Activity3.py
+++ Stuff-from-APP(24)/U1Activties.py/Activity3.py
@@ -1,25 +1,3 @@
-# 1) Print the full dictionary
-# 2) Add an additional country to the dictionary (Should now be 11) and print
-# out what country you added by referencing the dictionary (Do not type
-# it in as a string)
-# 3) Delete the first entry in your dictionary
-# 4) Print the full dictionary again
-# 5) Use the pop() method to delete the second item in your dictionary
-# 6) Print the full dictionary again
-
-#Canada CAN
-# Afghanistan	AFG	
-# Albania		ALB	
-# Algeria		DZA	
-# American Samoa	ASM	
-# Andorra	AND	
-# Angola	GO	
-# Anguilla	AIA	
-# Antarctica	ATA	
-# Barbuda	ATG	
-# Argentina		ARG	
-# Armenia		ARM	
-
 full_dict = {"Afghanistan": "AFG", "Albania": "ALB", "Algeria": "DZA", 
              "American Samoa": "ASM", "Andorra": "AND", "Angola": "GO", 
              "Anguilla": "AIA", "Antarctica": "ATA", "Barbuda": "ATG", 
