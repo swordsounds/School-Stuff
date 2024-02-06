51a6486805b840d037bb903840695999daf87300
swordsounds <126785544+swordsounds@users.noreply.github.com>
2024-02-06 10:44:20 -0330

    Things

---
+++ Stuff-from-APP(24)/U1Activties.py/Activity2.py
@@ -0,0 +1,20 @@
+# Exercise 1: Create a basic dictionary with
+# integer keys
+#  Create a python dictionary that will store the names of 5 animals, each with
+# an integer key; starting with 1
+# Exercise 2: Create another dictionary with
+# string keys
+#  Create a python dictionary that will store the names of 5 countries, and
+# their corresponding capital cities.
+# Exercise 3: Create a dictionary using the
+# built-in dict() function
+#  Create a python dictionary that will store the names of 5 vegetables, each
+# with an integer key; starting with 1. Make sure to use the dict() function
+
+my_dict = {1: "Apple", 2: "Banana", 3: "Orange", 4: "Mango"}
+
+animal_dict = {1: "Dog", 2: "Cat", 3: "Fish", 4: "Elephant", 5: "Horse"}
+
+string_dict = {"Ice": 121, "Tiger": 435, "Lion": 234, "Elephant": 345}
+
+country_dict = {"USA": "Washington D.C", "UK": "London", "Canada": "Ottawa", "France": "Paris", "Russia": "Moscow"}
--- Stuff-from-APP(24)/disctionary_playground.py
+++ Stuff-from-APP(24)/disctionary_playground.py
@@ -3,3 +3,4 @@ dict = {"name": "John", "age": 30, "city": "New York"}
 for key, value in dict.items():
     print(key, value)
 
+{x: x**2 for x in (2, 4, 6)}
