f0206a3d7bbdeb990166cf0621046596124ef880
swordsounds <126785544+swordsounds@users.noreply.github.com>
2024-02-08 11:48:00 -0330

    Doing more nonsense

--- Stuff-from-APP(24)/Labs/Lab1.py
+++ Stuff-from-APP(24)/Labs/Lab1.py
@@ -25,36 +25,48 @@
 # • Create a function grade_summary that takes a dictionary of student grades and prints a
 # summary: the total number of students, the highest grade, the lowest grade, and the
 # average grade.
+letters = {"A": 90, "B": 80, 
+           "C": 70, "D": 60, 
+           "E": 50, "F": 0}
 
-student_grades = {"John": 88, "Jane": 92, "Molly": "A", "Mary": "B", "Jake": 89}
+def main():
+    student_grades = {"John": 88, "Jane": 92, "Molly": "A", "Mary": "B", "Jake": 89}
+    updated_dict(student_grades)
+    delete_student(student_grades)
+    good_grades(student_grades)
+    grade_summary(student_grades)
+    print(student_grades)
 
-student_grades.update({"Mike": 0, "Dan": 50})
+def letter_grades(x):
+    for key in letters.keys():
+        if letters[key] == x:
+            letters[key] += grade_summary()
 
-student_grades["John"] = 22
+def updated_dict(student_grades):
+    student_grades.update({"John": 90, "Dan": 89})
 
-del student_grades["Mike"]
-
-print(student_grades.pop("Mary"))
-
-for key, value in student_grades.items():
-    print(key, value)
-    if value >= 90:
-        print(f"{key} has a grade of A or above 90!")
-
-def student_loop(student_grades):
-    for key, value in student_grades.items():
-        print(key, value)
-        if value >= 90:
-            print(f"{key} has a grade of A or above 90!")
+def delete_student(student_grades):
+    del student_grades["John"]
+def good_grades(student_grades):
+    for key in student_grades.keys():
+        try:
+            if student_grades[key] >= 90:
+                print(f"{key} has a grade of A or above 90!")
+        except TypeError:
+            letter_grades(student_grades(key))
 
 def grade_summary(student_grades):
-    total_students = len(student_grades)
-    highest_grade = max(student_grades.values())
-    lowest_grade = min(student_grades.values())
-    average_grade = sum(student_grades.values()) / len(student_grades)
-    print(f"Total Students: {total_students}")
-    print(f"Highest Grade: {highest_grade}")
-    print(f"Lowest Grade: {lowest_grade}")
-    print(f"Average Grade: {average_grade}")
+    try:
+        total_students = len(student_grades)
+        highest_grade = max(student_grades.values())
+        lowest_grade = min(student_grades.values())
+        average_grade = sum(student_grades.values()) / len(student_grades)
+        print(f"Total Students: {total_students}")
+        print(f"Highest Grade: {highest_grade}")
+        print(f"Lowest Grade: {lowest_grade}")
+        print(f"Average Grade: {average_grade}")
+    except TypeError:
+        print("Idk")
 
-student_loop(student_grades)
+if __name__ == "__main__":
+    main()
