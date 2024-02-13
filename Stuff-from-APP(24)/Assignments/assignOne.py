a804e4183da4c7b0e8956167352576d14601be80
swordsounds <126785544+swordsounds@users.noreply.github.com>
2024-02-13 10:57:45 -0330

    Finishing touches of the first assignment

--- Stuff-from-APP(24)/Assignments/assignOne.py
+++ Stuff-from-APP(24)/Assignments/assignOne.py
@@ -15,28 +15,54 @@
 # Write a program that computes and displays the Scrabble™ score for a word.
 # Create a dictionary that maps from letters to point values. Then use the dictionary to
 # compute the score.
-# A Scrabble™ board includes some squares that multiply the value of a letter
-# or the value of an entire word. We will ignore these squares in this exercise.
+
 
 
 from PyDictionary import PyDictionary
-dictionary = PyDictionary()
 
-def is_word_real(word):
-    try:
-        return bool (dictionary.meaning(word))
-    except:
-        return False
+dictionary = PyDictionary()
 
 letters = {"A": 1, "B": 3, "C": 3, "D": 2, "E": 1, "F": 4, "G": 2, "H": 4, "I": 1, 
            "J": 8, "K": 5, "L": 1, "M": 3, "N": 1, "O": 1, "P": 3, "Q": 10, "R": 1, 
            "S": 1, "T": 1, "U": 1, "V": 4, "W": 4, "X": 8, "Y": 4, "Z": 10}
+
+score_board = {}
+
+total = 0
+def menu():
+    print("Welcome to the Scrabble the game!")
+    print("Press 1 to exit...")
+
 def main():
-    user_input = input("Please enter a word: ").upper()
-    if is_word_real(user_input):
-        score = 0
-        for letter in user_input:
-            score += letters.get(letter)
-        print(f"Your score is {score}")
+    menu()
+    while True:
+        try:
+            print("--------")
+            for key, value in score_board.items():
+                print(f"{key}: {value}")
+            print("--------")
+            user_input = input("Please enter a word: ").upper()
+            if is_word_real(user_input):
+                score_counter(user_input)
+                print(total_score(total))
+        except:
+            print("Please enter a word!")
+
+def is_word_real(word):
+    try:
+        return bool(dictionary.meaning(word))
+    except:
+        return False
+    
+def score_counter(word):
+    score = 0
+    for letter in word:
+        score += letters.get(letter)
+    score_board[word] = score
+def total_score(total):
+    for value in score_board.values():
+        total += value
+    return total
+
 if __name__ == "__main__":
     main()
