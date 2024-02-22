ddd0cfd68675c461ecf6c1b49963f3cc1fb5bfbb
swordsounds <126785544+swordsounds@users.noreply.github.com>
2024-02-22 12:52:01 -0330

    Did the assignment along with other stuff

--- Stuff-from-APP_24/Assignments/assignTwo.py
+++ Stuff-from-APP_24/Assignments/assignTwo.py
@@ -1,35 +1,29 @@
-# In this assignment you will write a recursive function that determines whether or not a string is
-# a palindrome.
-    # • The empty string is a palindrome, as is any string containing only one character
-    # • Any longer string is a palindrome if its first and last characters
-# match, and if the string formed by removing the first and last characters is also
-# a palindrome.
-
-# Write a main program that reads a string from the user. Use your recursive function
-# to determine whether or not the string is a palindrome. Then display an appropriate
-# message for the user
-
-class Node:
-    def __init__(self, val):
-        self.value = val
-        self.left = None
-        self.right = None
+#This is the functuon to check if word is a palindrome
+def palindromeCheck(string):
+    #Base case
+    if len(string) == 0:
+        return "is a palindrome"
+    #Condition to check the first and last letter
+    elif string[0] == string[-1]:
+        #Recursive step that checkpoints the letters inbetween what was already checked
+        #To then make the first and last letter of that, to then be checked, so on and so forth
+        return palindromeCheck(string[1:-1])
+    else:
+        return "not a palindrome"
     
-    def preorder(node):
-        if node:
-            print(node.value)
-            node.left.preorder()
-            node.right.preorder()
-    def insert_node(self, val):
-        if val < self.value:
-            if self.left:
-                self.left = Node(val)
-            else:
-                self.left.insert(val)
-        else:
-            if self.right:
-                self.right = Node(val)
-            else:
-                self.right.insert(val)
+#-----------------TEST CODE-------------------
+# strings = ["kayak", "taco cat", "racecar", 
+#          "never odd or even", 
+#          "step on no pets", "ufo tofu"]
 
-root = input("Enter a string: ")
+# for string in strings:
+#     if palindromeCheck(string.replace(" ", "")):
+#         print(string, "is a palindrome")
+#     else:
+#         print(string, "is not a palindrome")
+#---------------------------------------------
+    
+#Conditon to run if this is the main file
+if __name__ == "__main__":
+    #Prints whatever is returned from the function as it is called
+    print(palindromeCheck(input("Enter a word: ").replace(" ", "").lower()))
