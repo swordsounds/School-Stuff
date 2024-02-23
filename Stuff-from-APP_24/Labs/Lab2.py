6c8b65aad5bbb8964217dc2fc97ccbf8883df361
swordsounds <126785544+swordsounds@users.noreply.github.com>
2024-02-23 13:19:22 -0330

    Adding comments

--- Stuff-from-APP_24/Labs/Lab2.py
+++ Stuff-from-APP_24/Labs/Lab2.py
@@ -1,76 +1,67 @@
-# Task 1: Implement a Binary Tree Node
-#   • Define a Node class with three attributes: val, left, and right.
-#   • Initialize the node with a value, and set left and right children to None.
-# Task 2: Build a Binary Tree
-    # Using the Node class from Task 1, create the following binary tree:
-    #  3
-    # / \
-    # 1 4
-    #  \
-    #   2
-# Task 3: Implement Preorder, Inorder, and Postorder Traversals
-    # Write functions for preorder, inorder, and postorder traversals of the binary tree.
-    # Each function should print the values of the nodes in the order defined by the type of traversal.
-# Task 4: Implement Insertion
-    # Write a function to insert new nodes into the binary tree. For simplicity, insert nodes to ensure the tree
-    # remains a binary search tree (BST). In a BST, for every node, all values in the left subtree are less than
-    # the node’s value, and all values in the right subtree are greater.
-    # Insert the following values into your binary tree: 5, 0. Test your tree by performing an inorder traversal
-    # to print the values in ascending order.
-
+#Task 3: Implement traversals
 def postorder(node):
     if node:
-        postorder(node.leftChild)
-        postorder(node.rightChild)
-        print(node.data) 
+        postorder(node.left)
+        postorder(node.right)
+        print(node.value) 
 def preorder(node):
     if node:
-        print(node.data) 
-        preorder(node.leftChild)
-        preorder(node.rightChild)
+        print(node.value) 
+        preorder(node.left)
+        preorder(node.right)
 def inorder(node):
     if node:
-        inorder(node.leftChild)
-        print(node.data) 
-        inorder(node.rightChild)
+        inorder(node.left)
+        print(node.value) 
+        inorder(node.right)
 
-
+#Task 1: Implment Binary Tree Node
 class Node:
     #Initializes the nodes values alogn with each of its subtrees
-    def __init__(self, data):
-        self.data = data
-        self.leftChild = None
-        self.rightChild = None
+    def __init__(self, val):
+        self.value = val
+        self.left = None
+        self.right = None
 # Function to insert in BST
-    def insert(self, data):
+    def insert(self, val):
         # if value is lesser than the value of the parent node
-        if data < self.data:
-            if self.leftChild:
-                # if we still need to move towards the left subtree
-                self.leftChild.insert(data)
+        if val < self.value:
+            if self.left:
+                # if need to move towards the left subtree
+                self.left.insert(val)
             else:
-                self.leftChild = Node(data)
+                #Makes a new left subtree when parent Node is "full"
+                self.left = Node(val)
                 return
         # if value is greater than the value of the parent node        
         else:
-            if self.rightChild:
-                # if we still need to move towards the right subtree
-                self.rightChild.insert(data)
+            if self.right:
+                # if need to move towards the right subtree
+                self.right.insert(val)
             else:
-                self.rightChild = Node(data)
+                # Makes new right subtree when parent Node is "full"
+                self.right = Node(val) 
                 return
 
 # Creating root node
 root = Node(3)
 
 # Inserting values in BST
+# Task 2: Build a Binary Tree
 root.insert(1)
 root.insert(4)
 root.insert(2)
-root.insert(5)
 
+# Task 4: Insert values 5, 0
+root.insert(5)
+root.insert(0)
+
+#Prints out the traversals
+print("--Post Order--")
 postorder(root)
 print()
+print("--Pre Order--")
 preorder(root)
 print()
+print("--In Order--")
 inorder(root)
