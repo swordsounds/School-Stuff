315185b7f7a1b04931f1e50869a97648e9e9db31
swordsounds <126785544+swordsounds@users.noreply.github.com>
2024-03-19 14:43:48 -0230

    okay

--- Stuff-from-APP_24/Labs/Lab4.py
+++ Stuff-from-APP_24/Labs/Lab4.py
@@ -7,18 +7,22 @@
 
 def main():
     class Monitor:
+        #Initial attributes of cups
         def __init__(self, cups):
             self.cups = cups
-
+        #Method to detemine the output
         def caffiene(self):
             if self.cups < 2:
+                #raises a custum expectyion when the input is less than 2
                 raise Exception("NotEnoughCaffeineError")
             else:
                 return "Success"
 
-
+    #Initializes an objct of Monitor
     m = Monitor(int(input("Please enter cups of joe: ")))
+    #Prints the values that would be returned depending on the cups of coffee
     print(m.caffiene())
 
+#Runs the main function is the name of this module is "main"
 if __name__ == "__main__":
     main()
