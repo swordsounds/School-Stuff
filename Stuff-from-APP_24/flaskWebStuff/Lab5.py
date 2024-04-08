3eeee31024eb5150e9413d74e424b8a682ca7bcc
swordsounds <126785544+swordsounds@users.noreply.github.com>
2024-04-08 13:38:37 -0230

    Downloaded mtplotlib

--- Stuff-from-APP_24/flaskWebStuff/Lab5.py
+++ Stuff-from-APP_24/flaskWebStuff/Lab5.py
@@ -1,40 +1,42 @@
 from markupsafe import escape 
 from flask import Flask, request, render_template
 
+#Initialized the application
 app = Flask(__name__)
 
-# simple app that returns "Hello, World!" on the home page.
+# Simple app that returns "Hello, World!" on the home page.
 @app.route('/')
 def home():
-    return '<h1>Hello World!</h1>\n<a href="http://127.0.0.1:5000/hello/Jake">Hello</a>\n<a href="http://127.0.0.1:5000/number/123">Number</a>\n<a href="http://127.0.0.1:5000/farewell/">Farewell</a>\n<a href="http://127.0.0.1:5000/greet">Greet</a>' 
+    #Home route to display Hello World! in a heading one
+    return '<h1>Hello World!</h1>' 
 
-# Part 2: Creating Simple Dynamic Routes
-# Create a route /hello/<name> that greets the user by name.
+# Simple Dynamic Routes
+# Created a route /hello/<name> that greets the user by name.
 @app.route('/hello/<user>')
 def hello(user):
-    # Modify the /hello/<name> route to render the hello.html template instead of returning a
+    # Modified the /hello/<name> route to render the hello.html template instead of returning a
     # string.
     return render_template('hello.html', user=user)
 
-#  route /number/<int:n> that displays the number back to the user
+#  Route /number/<int:n> that displays the number back to the user
 @app.route('/number/<int:n>')
 def number(n):
     return '<h1>{}</h1>'.format(n)
 
-# Create a route /greet that uses a query parameter name to display a greeting. If no name is
-# given, it defaults to "Guest".
-@app.route('/greet')
-@app.route('/greet/<name>', defaults={'username': 'guest'})
+# Created a route /greet that uses a query parameter name to display a greeting.
+#  If no name is given, it defaults to "Guest".
+@app.route('/greet', defaults={'name': 'guest'})
+@app.route('/greet/<name>')
 def greeting(name):
     return render_template("simpleGreetingPage.html", user=name)
 
-#  Farewell Route:
-# o Add a dynamic route /farewell/<name> that says goodbye to the user.
-# o Use a simple template to render the farewell message.
+# Farewell Route:
+# Added a dynamic route /farewell/<name> that says goodbye to the user.
 @app.route('/farewell/<name>')
 def farewell(name):
-    user = request.args.get('name')
-    return render_template('bye.html', user=user)
+    # Simple template to render the farewell message.
+    return render_template('bye.html', user=name)
 
+#If the file is the main file, if will call the app
 if __name__ == '__main__':
     app.run(debug=True)
