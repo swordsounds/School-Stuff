9f1efce850ed52beed779575464a776312edc263
swordsounds <126785544+swordsounds@users.noreply.github.com>
2024-05-10 15:24:41 -0230

    More things

--- Stuff-from-APP_24/Assignments/assignSix.py
+++ Stuff-from-APP_24/Assignments/assignSix.py
@@ -41,28 +41,57 @@ def HighestReleasesPerPlatform():
 # Top Selling Games: List the top 5 best-selling games of all time in a formatted table.
 def TopFiveBestSelling(df):
     df = df.sort_values(by=['Global_Sales'], ascending=False).head()
-    table = axes[2, 0].table(cellText=df.values, colLabels=df.columns, loc='center')
+    axes[2, 0].table(cellText=df.values, colLabels=df.columns, loc='center')
+
+# Genre Popularity: Create a pie chart to show the market share of each game genre based on global sales.
+def GenrePopularity():
+    x = []
+    y = []
+    for i in df['Genre'].unique():
+        y.append(sum((df.loc[df['Genre'] == i])['Global_Sales']))
+        x.append(i)
+    axes[0,1].pie(y, labels=x)
+    axes[0,1].set_title('Genre Popularity')
+
+# o Sales vs. Year Scatter Plot: Generate a scatter plot to explore the relationship between the year of release and global sales, including a trend line.
+def SalesVsYear():
+    x = []
+    y = []
+    for i in range(2000, 2024):
+        y.append(sum((df.loc[df['Year'] == float(i)])['Global_Sales']))
+        x.append(i)
+    axes[1,1].scatter(x, y)
+    axes[1,1].set_xlabel('Years')
+    axes[1,1].set_ylabel('Total Sales')
+    axes[1,1].set_title('Total Sales Per Year')
+
+
+# Sales Distribution by Platform: Use box plots to compare the global sales figures across different platforms, highlighting any outliers.
+def SalesPerPlatform():
+    x = []
+    y = []
+    for i in df['Platform'].unique():
+        y.append(sum((df.loc[df['Platform'] == i])['Global_Sales']))
+        x.append(i)
+    axes[2,1].scatter(x, y)
+    axes[2,1].set_xlabel('Platforms')
+    axes[2,1].set_ylabel('Total Sales')
+    axes[2,1].set_title('Total Sales Per Platform')
+    axes[2,1].set_xticklabels(axes[2,1].get_xticklabels(), rotation=45)
 
 TotalReleasePerYear()
 HighestReleasesPerPlatform()
 TopFiveBestSelling(df)
+GenrePopularity()
+SalesVsYear()
+SalesPerPlatform()
 plt.show()
 
 
 
 
 # o Annual Sales Trends: Compute and plot the total global sales per year to see how video game sales have evolved.
-# o Genre Popularity: Create a pie chart to show the market share of each game genre based on global sales.
 
+# Visualizations: Appropriately labeled plots with interpretations for each visualization included in the analysis.
 
-# o Sales vs. Year Scatter Plot: Generate a scatter plot to explore the relationship between
-# the year of release and global sales, including a trend line.
-# o Sales Distribution by Platform: Use box plots to compare the global sales figures across
-# different platforms, highlighting any outliers.
-
-# Code: A .py file containing your code
-# Visualizations: Appropriately labeled plots with interpretations for each visualization included in the
-# analysis.
-
-#  Include comments in your code to explain the purpose of each Python statement.
 #  For each plot, include a title, labels for x and y axes, and a legend if applicable.
