1b375a99803da7ebb2f265e0a8116e7d5b44d31b
Timny Li <126785544+swordsounds@users.noreply.github.com>
2024-12-11 21:21:31 -0330

    linked list insanity update

--- CompSci1001/playground.py
+++ CompSci1001/playground.py
@@ -3,15 +3,15 @@ class Node:
         self.__data = initdata
         self.__pointer = None
 
-    def set_node(self, node):
-        self.__pointer = node
-    def get_node(self):
-        return self.__pointer
-    
     def set_data(self, data):
         self.__data = data
     def get_data(self):
         return self.__data
+    
+    def set_next(self, node):
+        self.__pointer = node
+    def get_next(self):
+        return self.__pointer
 
 class LinkedList:
     def __init__(self):
@@ -27,49 +27,40 @@ class LinkedList:
         self.__size = value
     def get_size(self):
         return self.__size
+    
+    def add(self, value):
+        temp = Node(value)
+        temp.set_next(self.__head)
+        self.__head = temp
+        self.__size += 1
 
+    def append(self, value):
+        temp = Node(value)
+        current = self.__head
+
+        while current.get_next() != None:
+            current = current.get_next()
+        current.set_next(temp)
+        
     def search(self, value):
         current = self.__head
 
         while current != None:
             if current.get_data() == value:
                 return True
-            current = current.get_node()
+            current = current.get_next()
         return False
-
-    def add(self, value):
-        temp = Node(value)
-        temp.set_node(self.__head)
-        self.__head = temp
-        self.__size += 1
     
-    def append(self, value):
-        temp = Node(value)
-        current = self.__head
-
-        while current.get_node() != None:
-            current = current.get_node()
-        
-        current.set_node(temp)
-
     def print_list(self):
         current = self.__head
-
         while current != None:
             print(current.get_data(), end=" ")
-            current = current.get_node()
-
+            current = current.get_next()
 def main():
-    with open("README.md") as f:
-        print(f.read())
-
     x = LinkedList()
-    try:
-        x.add(1)
-        x.append(2)
-        print(x.search(3))
-        x.print_list()
-    except Exeption as e:
-        print(e)
+    x.add(1)
+    x.append(2)
+    print(x.search(2))
+    x.print_list()
 
 main()
